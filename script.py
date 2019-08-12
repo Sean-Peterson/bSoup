4a08a6384f79fa9da0c31e56b703c796819e69fd
Sean Peterson <seanpeterson11@gmail.com>
2019-08-12 16:43:18 -0500

    just ran the script for samsung and it grabbed about 50k data points. Wow

--- script.py
+++ script.py
@@ -31,7 +31,7 @@ for brand in allPhoneBrands:
 allBrandsPages = []
 phones = []
 # The below line should be commented out in order to run the whole program.
-allBrandsURLArray = ["apple-phones-48.php"]
+allBrandsURLArray = ["samsung-phones-9.php"]
 for brandURL in allBrandsURLArray:
     #Before we get all the links from each main page, we need to figure out how to get every page. This is found in the pages links at the bottom of the page.
     currentBrandName = str(brandURL).split("-")
@@ -106,143 +106,102 @@ for phone in phones:
 
     # to find Model, index numbers Name 0
     title=soup.find_all(attrs={"data-spec": "modelname"})
-    currentPhone.append("title")
     currentPhone.append(str(title))
     # to find Network, index numbers 1
     network=soup.find_all(attrs={"data-spec": "nettech"})
-    currentPhone.append("network")
     currentPhone.append(str(network))
     # to find Launch, index numbers 2,3
     year=soup.find_all(attrs={"data-spec": "year"})
-    currentPhone.append("year")
     currentPhone.append(str(year))
     status=soup.find_all(attrs={"data-spec": "status"})
-    currentPhone.append("status")
     currentPhone.append(str(status))
     # to find Body, index numbers  4-8
     dimensions=soup.find_all(attrs={"data-spec": "dimensions"})
-    currentPhone.append("dimensions")
     currentPhone.append(str(dimensions))
     weight=soup.find_all(attrs={"data-spec": "weight"})
-    currentPhone.append("weight")
     currentPhone.append(str(weight))
     build=soup.find_all(attrs={"data-spec": "build"})
-    currentPhone.append("build")
     currentPhone.append(str(build))
     sim=soup.find_all(attrs={"data-spec": "sim"})
-    currentPhone.append("sim")
     currentPhone.append(str(sim))
     bodyother=soup.find_all(attrs={"data-spec": "bodyother"})
-    currentPhone.append("bodyother")
     currentPhone.append(str(bodyother))
     # to find Display, index numbers 9-13
     displaytype=soup.find_all(attrs={"data-spec": "displaytype"})
-    currentPhone.append("displaytype")
     currentPhone.append(str(displaytype))
     displaysize=soup.find_all(attrs={"data-spec": "displaysize"})
-    currentPhone.append("displaysize")
     currentPhone.append(str(displaysize))
     displayresolution=soup.find_all(attrs={"data-spec": "displayresolution"})
-    currentPhone.append("displayresolution")
     currentPhone.append(str(displayresolution))
     displayprotection=soup.find_all(attrs={"data-spec": "displayprotection"})
-    currentPhone.append("displayprotection")
     currentPhone.append(str(displayprotection))
     displayother=soup.find_all(attrs={"data-spec": "displayother"})
-    currentPhone.append("displayother")
     currentPhone.append(str(displayother))
     # to find Platform, index numbers 14-17
     os=soup.find_all(attrs={"data-spec": "os"})
-    currentPhone.append("os")
     currentPhone.append(str(os))
     chipset=soup.find_all(attrs={"data-spec": "chipset"})
-    currentPhone.append("chipset")
     currentPhone.append(str(chipset))
     cpu=soup.find_all(attrs={"data-spec": "cpu"})
-    currentPhone.append("cpu")
     currentPhone.append(str(cpu))
     gpu=soup.find_all(attrs={"data-spec": "gpu"})
-    currentPhone.append("gpu")
     currentPhone.append(str(gpu))
     # to find Memory, index numbers 18,19
     memoryslot=soup.find_all(attrs={"data-spec": "memoryslot"})
-    currentPhone.append("memoryslot")
     currentPhone.append(str(memoryslot))
     internalmemory=soup.find_all(attrs={"data-spec": "internalmemory"})
-    currentPhone.append("internalmemory")
     currentPhone.append(str(internalmemory))
     # to find Main, index numbers Camera 20-22
     cam1modules=soup.find_all(attrs={"data-spec": "cam1modules"})
-    currentPhone.append("cam1modules")
     currentPhone.append(str(cam1modules))
     cam1features=soup.find_all(attrs={"data-spec": "cam1features"})
-    currentPhone.append("cam1features")
     currentPhone.append(str(cam1features))
     cam1video=soup.find_all(attrs={"data-spec": "cam1video"})
-    currentPhone.append("cam1video")
     currentPhone.append(str(cam1video))
     # to find Selfie, index numbers Camera 23-25
     cam2modules=soup.find_all(attrs={"data-spec": "cam2modules"})
-    currentPhone.append("cam2modules")
     currentPhone.append(str(cam2modules))
     cam2features=soup.find_all(attrs={"data-spec": "cam2features"})
-    currentPhone.append("cam2features")
     currentPhone.append(str(cam2features))
     cam2video=soup.find_all(attrs={"data-spec": "cam2video"})
     currentPhone.append(str(cam2video))
-    currentPhone.append("cam2video")
     # to find Sound, index numbers 26
     # LOUDSPEAKER does not have a data spec
     # 3.5mm jack does not have a data spec
     optionalother=soup.find_all(attrs={"data-spec": "optionalother"})
-    currentPhone.append("optionalother")
     currentPhone.append(str(optionalother))
     # to find Comms, index numbers 27-32
     wlan=soup.find_all(attrs={"data-spec": "wlan"})
-    currentPhone.append("wlan")
     currentPhone.append(str(wlan))
     bluetooth=soup.find_all(attrs={"data-spec": "bluetooth"})
-    currentPhone.append("bluetooth")
     currentPhone.append(str(bluetooth))
     gps=soup.find_all(attrs={"data-spec": "gps"})
-    currentPhone.append("gps")
     currentPhone.append(str(gps))
     nfc=soup.find_all(attrs={"data-spec": "nfc"})
-    currentPhone.append("nfc")
     currentPhone.append(str(nfc))
     radio=soup.find_all(attrs={"data-spec": "radio"})
-    currentPhone.append("radio")
     currentPhone.append(str(radio))
     usb=soup.find_all(attrs={"data-spec": "usb"})
-    currentPhone.append("usb")
     currentPhone.append(str(usb))
     # to find Features, index numbers 33-34
     sensors=soup.find_all(attrs={"data-spec": "sensors"})
-    currentPhone.append("sensors")
     currentPhone.append(str(sensors))
     featuresother=soup.find_all(attrs={"data-spec": "featuresother"})
-    currentPhone.append("featuresother")
     currentPhone.append(str(featuresother))
     # to find Battery, index numbers 35
     batdescription1=soup.find_all(attrs={"data-spec": "batdescription1"})
-    currentPhone.append("batdescription1")
     currentPhone.append(str(batdescription1))
     # CHARGING does not have a data spec 36-37
     battalktime1=soup.find_all(attrs={"data-spec": "battalktime1"})
-    currentPhone.append("battalktime1")
     currentPhone.append(str(battalktime1))
     batmusicplayback1=soup.find_all(attrs={"data-spec": "batmusicplayback1"})
-    currentPhone.append("batmusicplayback1")
     currentPhone.append(str(batmusicplayback1))
     # to find MISC, index numbers 38-40
     colors=soup.find_all(attrs={"data-spec": "colors"})
-    currentPhone.append("colors")
     currentPhone.append(str(colors))
     models=soup.find_all(attrs={"data-spec": "models"})
-    currentPhone.append("models")
     currentPhone.append(str(models))
     price=soup.find_all(attrs={"data-spec": "price"})
-    currentPhone.append("price")
     currentPhone.append(str(price))
     # to find Tests, index numbers
     # No tests have data specs
@@ -266,25 +225,23 @@ for phone in phones:
 wb = Workbook()
 sheet = wb.add_sheet("apple")
 
-
-first = 0
-second = 1
+headers = ["title", "network", "year", "status", "dimensions", "weight", "build", "sim", "body other", "display type", "display size", "display resolution", "display protection", "display other", "os", "chipset", "cpu", "gpu", "memory slot", "internal memory", "cam 1 modules", "cam 1 features", "cam 1 video", "cam 2 modules", "cam 2 features", "cam 2 video", "optional other", "wlan", "bluetooth", "gps", "nfc", "radio", "usb", "sensors", "features other", "battery description", "battery talk time", "battery music playback", "colors", "models", "price"]
+i = 0
+for header in headers:
+    row = 0
+    column = i
+    sheet.write(row,column,header)
+    i+=1
+starting = 1
 for phone in allPhonesData:
     j = 0
-    flipper = 0
     for spec in phone:
         column = j
-        if flipper == 0:
-            row = first
-            sheet.write(row,column,spec)
-            flipper = 1
-        else:
-            row = second
-            sheet.write(row,column,spec)
-            j+= 1
-            flipper = 0
-    first += 2
-    second += 2
+        row = starting
+        sheet.write(row,column,spec)
+        j+= 1
+        flipper = 0
+    starting += 1
 wb.save('GSM_phones_data.xls')
 
 
