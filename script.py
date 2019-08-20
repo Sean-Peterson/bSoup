a3ab7762cf5b9cb22210d96f25d41bea79eb25c3
Sean Peterson <seanpeterson11@gmail.com>
2019-08-20 09:28:51 -0500

    add working, but it keeps erroring out because of the connection. Maybe need to do just one brand at a time? Only call once?

--- script.py
+++ script.py
@@ -33,7 +33,7 @@ allBrandsPages = []
 phones = []
 # The below line should be commented out in order to run the whole program.
 for brandURL in allBrandsURLArray:
-    time.sleep(.333)
+    # time.sleep(.333)
     #Before we get all the links from each main page, we need to figure out how to get every page. This is found in the pages links at the bottom of the page.
     currentBrandName = str(brandURL).split("-")
     print(currentBrandName[0])
@@ -55,7 +55,7 @@ for brandURL in allBrandsURLArray:
             phones.append("https://www.gsmarena.com/"+string2[0])
     if totalBrandPages is not None:
         for i in totalBrandPages.descendants:
-            time.sleep(.333)
+            # time.sleep(.333)
             string = str(i)
             if currentBrandName[0]+"-phones" not in string:
                 pass
@@ -85,11 +85,19 @@ sheet0 = wb.add_sheet("phones")
 sheet1 = wb.add_sheet("phones2")
 sheet2 = wb.add_sheet("phones3")
 sheet3 = wb.add_sheet("phones4")
+sheet4 = wb.add_sheet("phones5")
+sheet5 = wb.add_sheet("phones6")
+sheet6 = wb.add_sheet("phones7")
+sheet7 = wb.add_sheet("phones8")
 i = 0
 starting = 1
 starting2 = 1
 starting3 = 1
 starting4 = 1
+starting5 = 1
+starting6 = 1
+starting7 = 1
+starting8 = 1
 counter = 0
 headers = ["title", "network", "year", "status", "dimensions", "weight", "build", "sim", "body other", "display type", "display size", "display resolution", "display protection", "display other", "os", "chipset", "cpu", "gpu", "memory slot", "internal memory", "cam 1 modules", "cam 1 features", "cam 1 video", "cam 2 modules", "cam 2 features", "cam 2 video", "optional other", "wlan", "bluetooth", "gps", "nfc", "radio", "usb", "sensors", "features other", "battery description", "battery talk time", "battery music playback", "colors", "models", "price"]
 for header in headers:
@@ -101,7 +109,7 @@ for header in headers:
     sheet3.write(row,column,header)
     i+=1
 for phone in phones:
-    time.sleep(.333)
+    # time.sleep(.333)
     currentPhone = []
     currentPhoneStrings = []
     page=urllib.urlopen(phone)
@@ -220,38 +228,66 @@ for phone in phones:
             currentPhoneStrings.append(str(currentString))
     # This is the part of the script to create an XLS using the data
 
-    for phone in currentPhoneStrings:
-        j = 0
-        for spec in phone:
-            counter += 1
-            if counter < 60000:
-                print(counter)
-                column = j
-                row = starting
-                sheet0.write(row,column,spec)
-                j+= 1
-                starting += 1
-            elif counter >= 60000 and counter < 90000:
-                print(counter)
-                column = j
-                row = starting2
-                sheet1.write(row,column,spec)
-                j+= 1
-                starting2 += 1
-            elif counter >= 90000 and counter < 120000:
-                print(counter)
-                column = j
-                row = starting2
-                sheet1.write(row,column,spec)
-                j+= 1
-                starting3 += 1
-            else:
-                print(counter)
-                column = j
-                row = starting2
-                sheet1.write(row,column,spec)
-                j+= 1
-                starting4 += 1
+    j = 0
+    for spec in currentPhoneStrings:
+        counter += 1
+        starting += 1
+        if counter < 60000:
+            print(counter)
+            column = j
+            row = starting
+            sheet0.write(row,column,spec)
+            j+= 1
+            # starting += 1
+        elif counter >= 60000 and counter < 120000:
+            print(counter)
+            column = j
+            row = starting2
+            sheet1.write(row,column,spec)
+            j+= 1
+            # starting2 += 1
+        elif counter >= 120000 and counter < 180000:
+            print(counter)
+            column = j
+            row = starting3
+            sheet2.write(row,column,spec)
+            j+= 1
+            # starting3 += 1
+        elif counter >= 180000 and counter < 240000:
+            print(counter)
+            column = j
+            row = starting4
+            sheet3.write(row,column,spec)
+            j+= 1
+            # starting4 += 1
+        elif counter >= 240000 and counter < 300000:
+            print(counter)
+            column = j
+            row = starting5
+            sheet4.write(row,column,spec)
+            j+= 1
+            # starting5 += 1
+        elif counter >= 300000 and counter < 360000:
+            print(counter)
+            column = j
+            row = starting6
+            sheet5.write(row,column,spec)
+            j+= 1
+            # starting6 += 1
+        elif counter >= 360000 and counter < 420000:
+            print(counter)
+            column = j
+            row = starting7
+            sheet6.write(row,column,spec)
+            j+= 1
+            # starting7 += 1
+        else:
+            print(counter)
+            column = j
+            row = starting8
+            sheet7.write(row,column,spec)
+            j+= 1
+            # starting8 += 1
 
 
     # allPhonesData.append(currentPhoneStrings)
